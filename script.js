9dc2b92f50cfedee1ab05f2e7c52908638136b31
Marshall-Ku <taengoo890309@naver.com>
2019-04-22 13:27:47 +0900

    modified

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 document.addEventListener("keydown", function(e) {
     const alert = document.getElementById("alert"),
           how = "How about using event.",
-          ins = "instead?";
+          ins = "instead?",
+          alt = e.altKey,
+          ctrl = e.ctrlKey,
+          shift = e.shiftKey,
+          meta = e.metaKey,
+          otp = ".output";
     e.preventDefault();
 
     document.body.classList.add("entered");
 
-    document.getElementById("key").querySelector(".output").innerText = e.key;
-    document.getElementById("location").querySelector(".output").innerText = e.location;
-    document.getElementById("which").querySelector(".output").innerText = e.which;
-    document.getElementById("code").querySelector(".output").innerText = e.code;
+    document.getElementById("key").querySelector(otp).innerText = e.key;
+    document.getElementById("location").querySelector(otp).innerText = e.location;
+    document.getElementById("which").querySelector(otp).innerText = e.which;
+    document.getElementById("code").querySelector(otp).innerText = e.code;
 
     " "===e.key
-    ?(document.getElementById("key").querySelector(".output").classList.add("none"),document.getElementById("key").querySelector(".output").innerText="(blank)")
-    :(document.getElementById("key").querySelector(".output").classList.remove("none"),document.getElementById("key").querySelector(".output").innerText=e.key);
+    ?(document.getElementById("key").querySelector(otp).classList.add("none"),document.getElementById("key").querySelector(otp).innerText="(blank)")
+    :(document.getElementById("key").querySelector(otp).classList.remove("none"),document.getElementById("key").querySelector(otp).innerText=e.key);
 
-    e.altKey||e.ctrlKey||e.shiftKey||e.metaKey
+    alt||ctrl||shift||meta
     ?(
-        e.altKey&&(alert.innerHTML=`${how}altKey ${ins}`),
-        e.ctrlKey&&(alert.innerHTML=`${how}ctrlKey ${ins}`),
-        e.shiftKey&&(alert.innerHTML=`${how}shiftKey ${ins}`),
-        e.metaKey&&(alert.innerHTML=`${how}metaKey ${ins}`),
+        alt&&(alert.innerHTML=`${how}altKey ${ins}`),
+        ctrl&&(alert.innerHTML=`${how}ctrlKey ${ins}`),
+        shift&&(alert.innerHTML=`${how}shiftKey ${ins}`),
+        meta&&(alert.innerHTML=`${how}metaKey ${ins}`),
+        shift&&"Shift"!==e.key&&(alert.innerHTML=`You pressed the key with Shift. Only event.key will detects it.`),
         alert.classList.add("sp")
     )
     :alert.classList.contains("sp")&&(
@@ -53,7 +59,7 @@ Array.from(document.querySelectorAll(".copybtn")).forEach(a => {
               pop = document.getElementById("pop");
 
         pop.classList.remove("reveal");
-        input.value = a.parentNode.querySelector(".output").innerText;
+        input.value = a.parentNode.querySelector(otp).innerText;
         input.select();
         document.execCommand("copy");
         pop.classList.add("reveal");
